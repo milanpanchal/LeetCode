93799887e9f04a16d7ddd234f7473bd275d91663
Milan Panchal <sam07it22@gmail.com>
2020-06-10 23:58:34 +0530

    Problem statement for June Challenge - Day 10 Search Insert Position

---
+++ Swift/June Challenge/10_SearchInsertPosition.playground/Contents.swift
@@ -0,0 +1,38 @@
+/*:
+#   Search Insert Position
+
+https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3356/
+ 
+ ---
+
+### Problem Statement:
+ 
+ Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+ You may assume no duplicates in the array.
+
+### Example:
+
+ ```
+ Input: [1,3,5,6], 5
+ Output: 2
+
+ Input: [1,3,5,6], 2
+ Output: 1
+
+ Input: [1,3,5,6], 7
+ Output: 4
+
+ Input: [1,3,5,6], 0
+ Output: 0
+
+ ```
+
+ */
+
+import UIKit
+
+func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+
+    
+}
