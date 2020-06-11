87ed112d5bc9731364cfa9174f215778f0da8c5c
Milan Panchal <sam07it22@gmail.com>
2020-06-11 17:43:09 +0530

    Problem statement for June Challenge - Day 11 Sort Colors

---
+++ Swift/June Challenge/11_SortColors.playground/Contents.swift
@@ -0,0 +1,36 @@
+/*:
+# Sort Colors
+  https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3357/
+
+ ---
+
+### Problem Statement:
+
+ Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+ Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+### Example:
+
+ ```
+ Input: [2,0,2,1,1,0]
+ Output: [0,0,1,1,2,2]
+
+ ```
+ 
+ ### Note:
+ You are not suppose to use the library's sort function for this problem.
+
+
+ ### Follow up:
+ + A rather straight forward solution is a two-pass algorithm using counting sort.
+ + First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+ + Could you come up with a one-pass algorithm using only constant space?
+ 
+ */
+
+import UIKit
+
+func sortColors(_ nums: inout [Int]) {
+    
+}
