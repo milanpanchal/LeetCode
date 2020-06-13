451eb6d4ca3de09c34cc3f5f59b79f1d0a3e025d
Milan Panchal <sam07it22@gmail.com>
2020-06-13 23:45:24 +0530

    Problem statement for June Challenge - Day 13: Largest Divisible Subset

---
+++ Swift/June Challenge/13_LargestDivisibleSubset.playground/Contents.swift
@@ -0,0 +1,34 @@
+/*:
+# Largest Divisible Subset
+  https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3359/
+
+ ---
+
+### Problem Statement:
+
+ Given a set of `distinct` positive integers, find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies:
+
+ ```Si % Sj = 0 or Sj % Si = 0.```
+
+ If there are multiple solutions, return any subset is fine.
+
+
+### Example:
+
+ ```
+ Input: [1,2,3]
+ Output: [1,2] (of course, [1,3] will also be ok)
+
+ Input: [1,2,4,8]
+ Output: [1,2,4,8]
+
+ ```
+
+ 
+ */
+
+import UIKit
+
+func largestDivisibleSubset(_ nums: [Int]) -> [Int] {
+    
+}
