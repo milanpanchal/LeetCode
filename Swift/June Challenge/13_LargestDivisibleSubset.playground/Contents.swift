d3c41460e6ff1db43e747933802bc9310c0f1fd4
Milan Panchal <sam07it22@gmail.com>
2020-06-14 19:03:18 +0530

    feat: June Challenge - Day 13: Largest Divisible Subset

--- Swift/June Challenge/13_LargestDivisibleSubset.playground/Contents.swift
+++ Swift/June Challenge/13_LargestDivisibleSubset.playground/Contents.swift
@@ -29,6 +29,79 @@
 
 import UIKit
 
-func largestDivisibleSubset(_ nums: [Int]) -> [Int] {
-    
+
+//41 / 41 test cases passed.
+//Status: Accepted
+//Runtime: 104 ms
+//Memory Usage: 21.4 MB
+
+class Solution {
+    func largestDivisibleSubset(_ nums: [Int]) -> [Int] {
+        
+        guard nums.count > 1 else {
+            return nums
+        }
+        
+        let sortedNums = nums.sorted()
+        var dp = Array(repeating: 0, count: sortedNums.count)
+        var prev = Array(repeating: -1, count: sortedNums.count)
+        var maxIndex = 0
+        
+        for i in 1..<sortedNums.count {
+            for j in 0..<i {
+                if (sortedNums[i] % sortedNums[j] == 0),
+                    (dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+                }
+            }
+            
+            // Update maxSubset size
+            if dp[i] > dp[maxIndex] {
+                maxIndex = i
+            }
+            
+        }
+        
+        var maxSubset = [Int]()
+        while maxIndex >= 0 {
+            maxSubset.append(sortedNums[maxIndex])
+            maxIndex = prev[maxIndex]
+        }
+        
+        return maxSubset
+    }
+}
+//
+//largestDivisibleSubset([1,2,3])             // [1, 2] or [1, 3]
+//largestDivisibleSubset([1, 16, 7, 8, 4])    // [1, 16, 8, 4]
+//largestDivisibleSubset([2, 4, 3, 8])        // [2, 4, 8]
+
+class Solution2 {
+    func largestDivisibleSubset(_ nums: [Int]) -> [Int] {
+        let nums = nums.sorted()
+        guard !nums.isEmpty else { return [] }
+        var dict = [Int: [Int]]()
+        for i in 0..<nums.count {
+            dict[i] = [nums[i]]
+        }
+        var resArray = [nums[0]]
+        for i in 1..<nums.count {
+            for j in 0..<i {
+                if nums[i] % nums[j] == 0 || nums[j] % nums[i] == 0 {
+                   var array = dict[j, default: []]
+                   array.append(nums[i])
+                   if dict[i, default: []].count < array.count {
+                       dict[i, default: []] = array
+                   }
+                    
+                   if resArray.count < array.count {
+                       resArray = array
+                   }
+                }
+            }
+        }
+        
+        return resArray
+    }
 }
