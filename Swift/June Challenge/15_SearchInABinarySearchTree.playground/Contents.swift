a296d2dd0fb8dee36aa0f9435d233d45e994b6bb
Milan Panchal <sam07it22@gmail.com>
2020-06-16 10:44:13 +0530

    feat: Solution for June Challenge 15: Search in a Binary Search Tree

--- Swift/June Challenge/15_SearchInABinarySearchTree.playground/Contents.swift
+++ Swift/June Challenge/15_SearchInABinarySearchTree.playground/Contents.swift
@@ -30,24 +30,49 @@
 
 import UIKit
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public var val: Int
- *     public var left: TreeNode?
- *     public var right: TreeNode?
- *     public init() { self.val = 0; self.left = nil; self.right = nil; }
- *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
- *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
- *         self.val = val
- *         self.left = left
- *         self.right = right
- *     }
- * }
- */
+
+// Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+
+//36 / 36 test cases passed.
+//Status: Accepted
+//Runtime: 172 ms
+//Memory Usage: 21.3 MB
 
 class Solution {
     func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
         
+        guard let node = root else {
+            return nil
+        }
+        
+        if node.val == val {
+            return node
+        } else if val > node.val {
+            return searchBST(node.right, val)
+        } else {
+            return searchBST(node.left, val)
+        }
+                
     }
 }
+
+let node = TreeNode(4)
+node.left = TreeNode(2)
+node.right = TreeNode(7)
+node.left?.left = TreeNode(1)
+node.left?.right = TreeNode(3)
+
+Solution().searchBST(node, 2)
