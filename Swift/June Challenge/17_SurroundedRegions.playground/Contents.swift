291ad763d96f3e79982ef18177873db0b0a8d14e
Milan Panchal <sam07it22@gmail.com>
2020-06-18 13:51:09 +0530

    feat: Solution for June Challenge: Day 17 Surrounded Regions

--- Swift/June Challenge/17_SurroundedRegions.playground/Contents.swift
+++ Swift/June Challenge/17_SurroundedRegions.playground/Contents.swift
@@ -36,6 +36,83 @@
  */
 import UIKit
 
-func solve(_ board: inout [[Character]]) {
+class Solution {
+    let boundryChar: Character = "O"
+    let specialChar: Character = "#"
+    
+    func solve(_ board: inout [[Character]]) {
+        
+        // Checking best case
+        if board.count == 0 || board[0].count == 0 { return }
+        
+        let totalRow  = board.count
+        let totalColumn = board[0].count
+        
+        // Checking Rows
+        for row in 0..<totalRow {
+            if board[row][0] == boundryChar { findBoundray(&board, row: row, column: 0)}
+            if board[row][totalColumn-1] == boundryChar { findBoundray(&board, row: row, column: totalColumn-1)}
+        }
+        
+        // Checking Columns
+        for column in 0..<totalColumn {
+            if board[0][column] == boundryChar { findBoundray(&board, row: 0, column: column)}
+            if board[totalRow-1][column] == boundryChar { findBoundray(&board, row: totalRow-1, column: column)}
+        }
+        print("board: \(board)")
+        
+        // Replacing Values
+        for row in 0..<totalRow {
+            for column in 0..<totalColumn {
+                if board[row][column] == specialChar {
+                    board[row][column] = boundryChar
+                } else if board[row][column] == boundryChar {
+                    board[row][column] = "X"
+                }
+            }
+        }
+        
+    }
+    
+    private func findBoundray(_ board: inout [[Character]], row: Int, column: Int) {
+        if row < 0 || column < 0 || row > board.count - 1 || column > board[0].count - 1 {
+            return
+        }
+        
+        if board[row][column] == boundryChar {
+            board[row][column] = specialChar
+        }
+        
+        if row > 0, board[row - 1][column] == boundryChar {
+            findBoundray(&board, row: row - 1, column: column)
+        }
+        
+        if row < board.count - 1, board[row + 1][column] == boundryChar {
+            findBoundray(&board, row: row + 1, column: column)
+        }
+        
+        if column > 0, board[row][column-1] == boundryChar {
+            findBoundray(&board, row: row, column: column-1)
+        }
+        
+        if column < board[0].count - 1, board[row][column+1] == boundryChar {
+            findBoundray(&board, row: row, column: column+1)
+        }
+        
+    }
     
 }
+
+
+var board:[[Character]] = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+
+let obj = Solution()
+obj.solve(&board)
+print("Ans: ", board)
+
+//var board2:[[Character]] = [["X","O","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+//obj.solve(&board2)
+//
+//var board3:[[Character]] = [["O"]]
+//obj.solve(&board3)
+//print(board3)
