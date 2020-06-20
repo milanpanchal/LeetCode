1fea37df472544626aa3302031a90695b91ae97d
Milan Panchal <sam07it22@gmail.com>
2020-06-21 00:27:08 +0530

    feat: Problem Statement for June Challenge: Day 20 - Permutation Sequence

---
+++ Swift/June Challenge/20_PermutationSequence.playground/Contents.swift
@@ -0,0 +1,47 @@
+/*:
+# Permutation Sequence
+  https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/541/week-3-june-15th-june-21st/3366/
+
+ ---
+
+### Problem Statement:
+
+ The set `[1,2,3,...,n]` contains a total of n! unique permutations.
+
+ By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+ + "123"
+ + "132"
+ + "213"
+ + "231"
+ + "312"
+ + "321"
+ 
+ Given `n` and `k`, return the `kth` permutation sequence.
+
+
+### Example:
+
+ ```
+ Input: n = 3, k = 3
+ Output: "213"
+
+ Input: n = 4, k = 9
+ Output: "2314"
+
+ ```
+ 
+ ### Notes:
+ + Given n will be between 1 and 9 inclusive.
+ + Given k will be between 1 and n! inclusive.
+ 
+ */
+
+
+import UIKit
+
+class Solution {
+    func getPermutation(_ n: Int, _ k: Int) -> String {
+        
+    }
+}
