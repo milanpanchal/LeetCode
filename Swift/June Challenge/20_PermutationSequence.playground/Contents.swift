e1e85ef509a61300280fb4bb590b60b38b7b8623
Milan Panchal <sam07it22@gmail.com>
2020-06-21 09:19:50 +0530

    feat: Solution for June Challenge: Day 20 - Permutation Sequence

--- Swift/June Challenge/20_PermutationSequence.playground/Contents.swift
+++ Swift/June Challenge/20_PermutationSequence.playground/Contents.swift
@@ -40,8 +40,40 @@
 
 import UIKit
 
+// 200 / 200 test cases passed.
+// Status: Accepted
+// Runtime: 4 ms
+// Memory Usage: 21.3 MB
+
 class Solution {
+    
+    // Helper function
+    private func factorials(_ n: Int) -> [Int] {
+        var resullt = [1]
+        for i in 1..<n {
+            resullt.append(resullt[i-1]*i)
+        }
+        return resullt
+    }
+    
     func getPermutation(_ n: Int, _ k: Int) -> String {
+        
+        var kMutable = k - 1
+        var numbers = (1...n).map{ String($0) }
+        var result = ""
+        
+        // print(factorials(n))
+        for c in factorials(n).reversed() {
+            result += numbers.remove(at: kMutable / c)
+            kMutable %= c
+        }
+        
+        return result
         
     }
 }
+
+let obj = Solution()
+obj.getPermutation(3, 3) // 213
+obj.getPermutation(5, 9) // 2314
+obj.getPermutation(1, 1) // 1
