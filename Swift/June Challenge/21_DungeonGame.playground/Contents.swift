81f8ed68416a22199b543533289807a6ab737823
Milan Panchal <sam07it22@gmail.com>
2020-06-22 11:41:32 +0530

    feat: Solution for June Challenge: Day 21 - Dungeon Game

--- Swift/June Challenge/21_DungeonGame.playground/Contents.swift
+++ Swift/June Challenge/21_DungeonGame.playground/Contents.swift
@@ -46,7 +46,36 @@ import UIKit
 class Solution {
     func calculateMinimumHP(_ dungeon: [[Int]]) -> Int {
         
-        return 0
+        guard dungeon.count > 0 else {
+            return 1
+        }
+        
+        let rows = dungeon.count
+        let columns = dungeon[0].count
+        
+        var dp = Array(repeating: Array(repeating: 0, count: columns), count: rows)
+
+        // Setup min value to enter before princess (P) Cell
+        dp[rows-1][columns-1] = max(1 - dungeon[rows-1][columns-1], 1)
+        
+        // Setting last column value
+        for i in stride(from: rows-2, through: 0, by: -1) {
+            dp[i][columns-1] = max(dp[i+1][columns-1] - dungeon[i][columns-1], 1)
+        }
+
+        // Setting last rows value
+        for j in stride(from: columns-2, through: 0, by: -1) {
+            dp[rows-1][j] = max(dp[rows-1][j+1] - dungeon[rows-1][j], 1)
+        }
+
+        // Setting remaining cells
+        for i in stride(from: rows-2, through: 0, by: -1) {
+            for j in stride(from: columns-2, through: 0, by: -1) {
+                dp[i][j] = max(min(dp[i+1][j],dp[i][j+1]) - dungeon[i][j], 1)
+            }
+        }
+        
+        return dp.first!.first!
     }
 }
 
