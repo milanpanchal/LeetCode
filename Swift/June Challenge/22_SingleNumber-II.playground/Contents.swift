249cad3ae15ef9c66170bcf1568a4305659c2dbf
Milan Panchal <sam07it22@gmail.com>
2020-06-22 23:28:25 +0530

    feat: Problem Statement for June Challenge: Day 22 - Single Number II

---
+++ Swift/June Challenge/22_SingleNumber-II.playground/Contents.swift
@@ -0,0 +1,39 @@
+/*:
+# Single Number II
+  https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3368/
+
+ ---
+
+### Problem Statement:
+
+ Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+### Example:
+
+ ```
+ Input: [2,2,3,2]
+ Output: 3
+
+ Input: [0,1,0,1,0,1,99]
+ Output: 99
+
+ ```
+
+ ### Notes:
+ + Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ 
+ */
+
+import UIKit
+
+class Solution {
+    func singleNumber(_ nums: [Int]) -> Int {
+        
+        return 0
+    }
+}
+
+let sol = Solution()
+sol.singleNumber([2,2,3,2]) // 3
+sol.singleNumber([0,1,0,1,0,1,99]) // 99
+
