335c36beda35592fe219d701552a73dd222f48ad
Milan Panchal <sam07it22@gmail.com>
2020-06-24 16:05:52 +0530

    feat: Problem Statement for June Challenge: Day 24 - Unique Binary Search Tree

---
+++ Swift/June Challenge/24_UniqueBinarySearchTrees.playground/Contents.swift
@@ -0,0 +1,39 @@
+/*:
+# Unique Binary Search Trees
+ 
+https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3370/
+
+ ---
+
+### Problem Statement:
+
+ Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+
+### Example:
+
+ ```
+ Input: 3
+ Output: 5
+
+ Given n = 3, there are a total of 5 unique BST's:
+
+ 1         3     3      2      1
+  \       /     /      / \      \
+   3     2     1      1   3      2
+  /     /       \                 \
+ 2     1         2                 3
+
+ ```
+ 
+ */
+
+import UIKit
+
+class Solution {
+    func numTrees(_ n: Int) -> Int {
+        
+    }
+}
+
+let sol = Solution()
+sol.numTrees(3) // 5
