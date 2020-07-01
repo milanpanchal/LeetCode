75cea57c9a643de9b176f8bf8c32168d3416336e
Milan Panchal <sam07it22@gmail.com>
2020-07-01 13:49:41 +0530

    feat: Solution for June Challenge: Day 30 - Word Search II

--- Swift/June Challenge/30_WordSearch-II.playground/Contents.swift
+++ Swift/June Challenge/30_WordSearch-II.playground/Contents.swift
@@ -35,13 +35,63 @@
 
 import UIKit
 
+// Note: Solution for this logic is not written by me. I've learnt on through leetcode submission.
 class Solution {
+    class TrieNode {
+        var word: String?
+        var children = [Character: TrieNode]()
+    }
+
     func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
-     
-        return []
+        let trieRoot = buildTrie(words)
+        
+        var board = board
+        var result = Set<String>()
+        for i in 0..<board.count {
+            for j in 0..<board[i].count {
+                dfs(&board, i, j, trieRoot, &result)
+            }
+        }
+        
+        return Array(result)
+    }
+    
+    func dfs(_ board: inout [[Character]], _ i: Int, _ j: Int, _ trieNode: TrieNode, _ result: inout Set<String>) {
+        if let word = trieNode.word {
+            result.insert(word)
+        }
+        
+        guard i >= 0 && j >= 0 && i < board.count && j < board[i].count else { return }
+        guard board[i][j] != "#" else { return }
+        
+        let char = board[i][j]
+        guard let nextTrieNode = trieNode.children[char] else { return }
+        
+        var board = board
+        board[i][j] = "#"
+        for (nexti, nextj) in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)] {
+            dfs(&board, nexti, nextj, nextTrieNode, &result)
+        }
+        board[i][j] = char
+    }
+    
+    func buildTrie(_ words: [String]) -> TrieNode {
+        let root = TrieNode()
+        for word in words {
+            var node = root
+            for char in word {
+                if node.children[char] == nil {
+                    node.children[char] = TrieNode()
+                }
+                node = node.children[char]!
+            }
+            node.word = word
+        }
+        return root
     }
 }
 
+
 let sol = Solution()
 sol.findWords([["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
-, ["oath","pea","eat","rain"])
+, ["oath","pea","eat","rain"]) // ["eat","oath"]
