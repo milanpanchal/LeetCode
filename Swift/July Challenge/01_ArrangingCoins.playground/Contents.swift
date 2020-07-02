4b590758a819ce3fdb00d836104e982f012752ab
Milan Panchal <sam07it22@gmail.com>
2020-07-02 10:40:29 +0530

    feat: Solution for July Challenge: Day 1 - Arranging Coins

--- Swift/July Challenge/01_ArrangingCoins.playground/Contents.swift
+++ Swift/July Challenge/01_ArrangingCoins.playground/Contents.swift
@@ -1,6 +1,6 @@
 /*:
-# Arranging Coins
-  https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3377/
+# 441. Arranging Coins
+  https://leetcode.com/problems/arranging-coins/
 
  ---
 
