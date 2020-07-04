bca9eaf19a372c9ed7090073d9732b1091c3a355
Milan Panchal <sam07it22@gmail.com>
2020-07-04 11:49:24 +0530

    feat: Solution for July Challenge: Day 3 - Prison Cells After N Days

--- Swift/July Challenge/03_PrisonCellsAfterNDays.playground/Contents.swift
+++ Swift/July Challenge/03_PrisonCellsAfterNDays.playground/Contents.swift
@@ -53,12 +53,85 @@
 
 import UIKit
 
+// 0: Current Vacant
+// 1: Current Occupied
+// 2: To be vacant
+// 3: To be occupied
+
 class Solution {
+    
+    //    258 / 258 test cases passed.
+    //    Status: Accepted
+    //    Runtime: 16 ms
+    //    Memory Usage: 21.1 MB
+    
     func prisonAfterNDays(_ cells: [Int], _ N: Int) -> [Int] {
-     
-        return []
+        
+        guard cells.count > 2 else { return cells }
+
+        var cells = cells
+        let cellCount = cells.count
+        let N = N % 14 == 0 ? 14 : N % 14
+
+        for _ in 0..<N {
+            
+            cells[0] = cells[0] == 1 ? 2 : 0
+            
+            for i in 1..<cellCount-1 {
+                if (cells[i-1] == 1 && cells[i+1] == 1) ||
+                    (cells[i-1] == 0 && cells[i+1] == 0) ||
+                    (cells[i-1] == 3 && cells[i+1] == 0) ||
+                    (cells[i-1] == 2 && cells[i+1] == 1) {
+                    cells[i] = cells[i] == 1 ? 1 : 3
+                } else {
+                    cells[i] = cells[i] == 0 ? 0 : 2
+                }
+            }
+            
+            cells[cellCount-1] = cells[cellCount-1] == 1 ? 2 : 0
+            
+            for i in 1..<cellCount-1 {
+                if cells[i] == 2 {
+                    cells[i] = 0
+                } else if cells[i] == 3 {
+                    cells[i] = 1
+                }
+            }
+            
+            // print(cells)
+        }
+        return cells
     }
 }
 
 let sol = Solution()
-sol.prisonAfterNDays([0,1,0,1,1,0,0,1], 7)
+sol.prisonAfterNDays([0,1,0,1,1,0,0,1], 7) // [0,0,1,1,0,0,0,0]
+sol.prisonAfterNDays([1,0,0,1,0,0,1,0], 1000000000) // [0,0,1,1,1,1,1,0]
+
+
+
+// Using extra array
+class Solution1 {
+    func prisonAfterNDays(_ cells: [Int], _ N: Int) -> [Int] {
+        guard cells.count > 2 else { return cells }
+        var result = cells
+        let optimizedN = (N - 1) % 14 + 1
+        for day in 0..<optimizedN {
+
+                var newState = result
+                newState[0] = 0
+                newState[7] = 0
+                for index in 1...result.count - 2 {
+                    if result[index - 1] == result[index + 1] {
+                        newState[index] = 1
+                    } else {
+                        newState[index] = 0
+                    }
+                }
+                result = newState
+            
+        }
+        
+        return result
+    }
+}
