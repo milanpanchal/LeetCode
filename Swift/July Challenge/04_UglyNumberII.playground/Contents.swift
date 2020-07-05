61e4d553b24e653e68f3cb088a305a3cf1fd43a9
Milan Panchal <sam07it22@gmail.com>
2020-07-05 10:51:16 +0530

    fix: Problem Statement for July Challenge: Day 4 - Ugly Number II

--- Swift/July Challenge/04_UglyNumberII.playground/Contents.swift
+++ Swift/July Challenge/04_UglyNumberII.playground/Contents.swift
@@ -6,7 +6,9 @@
 
 ### Problem Statement:
  Write a program to find the `n`-th ugly number.
-
+ 
+ Ugly numbers are positive numbers whose prime factors only include `2, 3, 5`.
+ 
 ### Example:
 
  ```
