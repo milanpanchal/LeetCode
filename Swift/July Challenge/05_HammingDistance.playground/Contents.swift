721bb2bd059501279c5c6856424b34e3930ff85c
Milan Panchal <sam07it22@gmail.com>
2020-07-05 22:41:56 +0530

    fix: Problem Statement for July Challenge: Day 5 - Hamming Distance

---
+++ Swift/July Challenge/05_HammingDistance.playground/Contents.swift
@@ -0,0 +1,41 @@
+/*:
+# Hamming Distance
+  https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3381/
+
+ ---
+
+### Problem Statement:
+ The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+ Given two integers x and y, calculate the Hamming distance.
+
+### Example:
+
+ ```
+ Input: x = 1, y = 4
+
+ Output: 2
+
+ Explanation:
+ 1   (0 0 0 1)
+ 4   (0 1 0 0)
+        ↑   ↑
+
+ The above arrows point to positions where the corresponding bits are different.
+ ```
+ 
+ ### Notes:
+ + 0 ≤ x, y < 2^31
+ 
+ */
+
+import UIKit
+
+class Solution {
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        
+    }
+}
+
+let sol = Solution()
+sol.hammingDistance(1, 4) // 2
