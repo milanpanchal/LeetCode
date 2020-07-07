df5bb434b912070dd030a1f6cdcbfb88f165f6ab
Milan Panchal <sam07it22@gmail.com>
2020-07-07 10:32:17 +0530

    fix: Problem Statement for July Challenge: Day 6 - Plus One

---
+++ Swift/July Challenge/06_PlusOne.playground/Contents.swift
@@ -0,0 +1,42 @@
+/*:
+# Plus One
+  https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3382/
+
+ ---
+
+### Problem Statement:
+
+ Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+ The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+ You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+
+### Example:
+
+ ```
+ Input: [1,2,3]
+ Output: [1,2,4]
+ Explanation: The array represents the integer 123.
+
+ Input: [4,3,2,1]
+ Output: [4,3,2,2]
+ Explanation: The array represents the integer 4321.
+
+ ```
+
+ */
+
+import UIKit
+
+class Solution {
+    func plusOne(_ digits: [Int]) -> [Int] {
+        var digits = digits
+        return digits
+    }
+}
+
+let sol = Solution()
+sol.plusOne([1,2,3]) // [1,2,4]
+sol.plusOne([4,3,2,1]) // [4,3,2,2]
