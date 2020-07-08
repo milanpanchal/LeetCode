16b6afbe89e9be910629aea617b19754844804bf
Milan Panchal <sam07it22@gmail.com>
2020-07-08 23:49:56 +0530

    feat: Solution for July Challenge: Day 8 - 3 Sum

--- Swift/July Challenge/08_3Sum.playground/Contents.swift
+++ Swift/July Challenge/08_3Sum.playground/Contents.swift
@@ -35,12 +35,127 @@
 
 import UIKit
 
-class Solution {
+// TLE - Time limit exception
+// Brute force solution
+class Solution1 {
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        let nums = nums.sorted()
+        
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                for k in j + 1..<nums.count {
+                    if nums[i] + nums[j] + nums[k] == 0 {
+                        if !result.contains([nums[i], nums[j], nums[k]]) {
+                            result.append([nums[i], nums[j], nums[k]])
+                        }
+                    }
+                }
+            }
+        }
+        print(result)
+        return result
+    }
+        
+}
+
+// Using twoSum function
+class Solution2 {
+    var result = [[Int]]()
+
     func threeSum(_ nums: [Int]) -> [[Int]] {
         
+        guard nums.count >= 3 else {
+            return []
+        }
+        
+        let nums = nums.sorted()
+
+        for i in 0..<nums.count-2 {
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue
+            }
+            twoSum(i + 1, nums.count - 1, nums, -nums[i])
+        }
+        
+        return result
+    }
+    
+    func twoSum(_ start: Int, _ end: Int, _ nums: [Int], _ target: Int) {
+        var start = start
+        var end = end
+        while start < end {
+            if nums[start] + nums[end] == target {
+                result.append([-target, nums[start], nums[end]])
+                start += 1
+                end -= 1
+                
+                while start < end && nums[start] == nums[start - 1] {
+                    start += 1
+                }
+                
+                while start < end && nums[end] == nums[end + 1] {
+                    end -= 1
+                }
+            }
+            else if nums[start] + nums[end] < target {
+                start += 1
+            }
+            else {
+                end -= 1
+            }
+        }
     }
 }
 
-let sol = Solution()
+// 313 / 313 test cases passed.
+// Status: Accepted
+// Runtime: 432 ms
+// Memory Usage: 23.4 MB
+
+class Solution {
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+      
+        let nums = nums.sorted()
+        var current = 0
+        
+        while current < (nums.count - 2) {
+            
+            var start = current + 1
+            var end = nums.count - 1
+            let firstValue = nums[current]
+            while start < end {
+                let sum = firstValue + nums[start] + nums[end]
+                
+                if sum == 0 {
+                    result.append([firstValue, nums[start], nums[end]])
+                }
+                    
+                if sum < 0 {
+                    let currentStart = start
+                    while start < end && nums[start] == nums[currentStart] {
+                        start += 1
+                    }
+                } else {
+                    let currentEnd = end
+                    while start < end && nums[end] == nums[currentEnd] {
+                        end -= 1
+                    }
+                }
+            }
+            
+            let currentIndex = current
+            while current < (nums.count - 2) && nums[current] == nums[currentIndex] {
+                current += 1
+            }
+        }
+        
+        return result
+    }
+
+}
+let sol = Solution2()
 sol.threeSum([-1,0,1,2,-1,-4]) // [[-1, 0, 1], [-1, -1, 2]]
+sol.threeSum([13,-14,-10,-4,4,4,0,-14,5,-9,-3,-10,14,7,-3,-4,-3,12,-14,2,-11,-6,0,-7,13,-2,-7,-11,-14,-13,5,14,-12,11,-13,-1,-8,2,0,4,1,4,10,-8,-11,-8,3,1,11,4,-12,8,5,-4,-14,-4,9,-13,-8,2,-11,12,-7,14,0,-5,-2,7,5,5,-3,13,-6,-8,-10,-10,-9,0,6,-12,11,2,11,1,13,4,12,-1,6,-11,-14,2,9,-6,1,-6,-4,14,-13,8,4,-1,6,6,-2,11,11,4,-4,-5,-1,-8,12,-13,1,10,7,-10,-14,-10,-5,-13,0,11])
 
