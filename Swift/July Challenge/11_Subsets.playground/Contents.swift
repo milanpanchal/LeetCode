e344cf53fb7518c8537f9179ee3d7d609230c99b
Milan Panchal <sam07it22@gmail.com>
2020-07-12 07:20:14 +0530

    feat: Problem Statement for July Challenge: Day 11 - Subsets

---
+++ Swift/July Challenge/11_Subsets.playground/Contents.swift
@@ -0,0 +1,42 @@
+/*:
+# Subsets
+  https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3387/
+
+ ---
+
+### Problem Statement:
+ Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+### Example:
+
+ ```
+ Input: nums = [1,2,3]
+ Output:
+ [
+   [3],
+   [1],
+   [2],
+   [1,2,3],
+   [1,3],
+   [2,3],
+   [1,2],
+   []
+ ]
+
+ ```
+ 
+ ### Notes:
+ + The solution set must not contain duplicate subsets.
+ 
+ */
+
+import UIKit
+
+class Solution {
+    func subsets(_ nums: [Int]) -> [[Int]] {
+        
+    }
+}
+
+let sol = Solution()
+sol.subsets([1,2,3])
