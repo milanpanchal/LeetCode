d8151743aeb5cd6b702913eace3710c104897b72
Milan Panchal <sam07it22@gmail.com>
2020-07-13 10:25:54 +0530

    feat: Problem Statement for July Challenge: Day 12 - Reverse Bits

---
+++ Swift/July Challenge/12_ReverseBits.playground/Contents.swift
@@ -0,0 +1,50 @@
+/*:
+# Reverse Bits
+  https://leetcode.com/explore/featured/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3388/
+
+ ---
+
+### Problem Statement:
+ Reverse bits of a given 32 bits unsigned integer.
+
+### Example 1:
+
+ ```
+ Input: 00000010100101000001111010011100
+ Output: 00111001011110000010100101000000
+
+ Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+
+ ```
+ 
+ ### Example 2:
+
+ ```
+ Input: 11111111111111111111111111111101
+ Output: 10111111111111111111111111111111
+ 
+ Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+ 
+ ```
+ 
+ ### Notes:
+ + Note that in some languages such as Java, there is no unsigned integer type. In this case, both input and output will be given as signed integer type and should not affect your implementation, as the internal binary representation of the integer is the same whether it is signed or unsigned.
+
+ + In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above the input represents the signed integer `-3` and the output represents the signed integer `-1073741825`.
+ 
+ ### Follow up:
+
+ If this function is called many times, how would you optimize it?
+
+ */
+
+import UIKit
+
+class Solution {
+    func reverseBits(_ n: Int) -> Int {
+        
+    }
+}
+
+let sol = Solution()
+sol.reverseBits(00000010100101000001111010011100)
