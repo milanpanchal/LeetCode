ad55640d8ec00017858bc715e94605f33192c173
Milan Panchal <sam07it22@gmail.com>
2020-07-16 16:24:09 +0530

    feat: Solution for July Challenge: Day 16 - Pow(x, n)

--- Swift/July Challenge/16_Pow(x,n).playground/Contents.swift
+++ Swift/July Challenge/16_Pow(x,n).playground/Contents.swift
@@ -32,16 +32,76 @@
 import UIKit
 
 class Solution {
+    
+    // Status: Time Limit Exceeded
+    func myPow1(_ x: Double, _ n: Int) -> Double {
+        
+        if n == 0 {
+            return 1
+        }
+        
+        let nAbs = abs(n)
+        
+        var xPow = x
+        for _ in 1..<nAbs {
+            xPow *= x
+        }
+        return n > 0 ? xPow : 1 / xPow
+    }
+    
     func myPow(_ x: Double, _ n: Int) -> Double {
         
+        if n == 0 {
+            return 1
+        }
+        
+        if n == 1 {
+            return x
+        }
+        
+        
+        let temp = myPow(x, n/2)
+        
+        if n % 2 == 0 {
+            return temp * temp
+        } else {
+            if n > 0 {
+                return x * temp * temp
+            } else {
+              return (temp * temp) / x
+            }
+        }
+        
     }
+    
+    // Without recursion
+    func myPow3(_ x: Double, _ n: Int) -> Double {
+        if n == 0 {
+            return 1
+        }
+        
+        var result: Double = 1
+        var temp = x
+        var count = abs(n)
+        while count >= 1 {
+            if count % 2 != 0 {
+                result *= temp
+            }
+            
+            temp *= temp
+            count = count / 2
+        }
+        
+        return n > 0 ? result : 1 / result
+    }
+
 }
 
 let sol = Solution()
 sol.myPow(2.00000, 10)  // 1024.00000
 sol.myPow(2.10000, 3)   // 9.26100
 sol.myPow(2.00000, -2)  // 0.25000
-//sol.myPow(0.00001, 2147483647) // TLE: Exception
+sol.myPow(0.00001, 2147483647) // TLE: Exception
 
 
 
