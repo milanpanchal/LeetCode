951f022a3f4109f048ba7c4fb4bb93fac4ea6d9a
Milan Panchal <sam07it22@gmail.com>
2020-07-22 20:09:44 +0530

    feat: Solution for July Challenge: Day 22 - Binary Tree Zigzag Level Order Traversal

--- Swift/July Challenge/22_BinaryTreeZigzagLevelOrderTraversal.playground/Contents.swift
+++ Swift/July Challenge/22_BinaryTreeZigzagLevelOrderTraversal.playground/Contents.swift
@@ -50,9 +50,70 @@ public class TreeNode {
 
 
 class Solution {
+    
+//    33 / 33 test cases passed.
+//    Status: Accepted
+//    Runtime: 20 ms
+//    Memory Usage: 20.6 MB
+
     func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else {
+            return []
+        }
         
-        return []
+        var result = [[Int]]()
+        
+        BFS(root, &result, 0)
+        
+        return result
+    }
+    
+    func BFS(_ node: TreeNode?, _ result: inout [[Int]], _ level: Int) {
+        guard let node = node else {
+            return
+        }
+        
+        if result.count < level + 1 {
+            result.append([Int]())
+        }
+        
+        if level % 2 == 0 {
+            result[level].append(node.val)
+        } else {
+            result[level].insert(node.val, at: 0)
+        }
+        
+        BFS(node.left, &result, level + 1)
+        BFS(node.right, &result, level + 1)
+    }
+}
+
+class Solution1 {
+    func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else {
+            return []
+        }
+        var results: [[Int]] = []
+        
+        var queue = [root]
+        var shouldReverse = false
+        while !queue.isEmpty {
+            let length = queue.count
+            var line: [Int] = []
+            for _ in 0..<length {
+                let current = queue.removeFirst()
+                line.append(current.val)
+                if let left = current.left {
+                    queue.append(left)
+                }
+                if let right = current.right {
+                    queue.append(right)
+                }
+            }
+            results.append(shouldReverse ? line.reversed() : line)
+            shouldReverse = !shouldReverse
+        }
+        return results
     }
 }
 
@@ -63,4 +124,5 @@ node.right?.left = TreeNode(15)
 node.right?.right = TreeNode(7)
 
 let sol = Solution()
-sol.zigzagLevelOrder(node)
+sol.zigzagLevelOrder(node) // [[3],[20,9],[15,7]]
+
