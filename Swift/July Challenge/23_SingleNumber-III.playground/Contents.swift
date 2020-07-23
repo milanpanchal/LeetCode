c8a38e2fa63648d35bc666fd25c45cd4f7933fcf
Milan Panchal <sam07it22@gmail.com>
2020-07-23 22:09:20 +0530

    feat: Problem Statementfor July Challenge: Day 23 - Single Number III

---
+++ Swift/July Challenge/23_SingleNumber-III.playground/Contents.swift
@@ -0,0 +1,36 @@
+/*:
+# Single Number III
+  https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3399/
+
+ ---
+
+### Problem Statement:
+
+ Given an array of numbers `nums`, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+
+### Example:
+
+ ```
+ Input:  [1,2,1,3,2,5]
+ Output: [3,5]
+
+ ```
+ 
+ ### Notes:
+ + The order of the result is not important. So in the above example, `[5, 3]` is also correct.
+ + Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+
+ */
+
+
+import UIKit
+
+class Solution {
+    func singleNumber(_ nums: [Int]) -> [Int] {
+        return []
+    }
+}
+
+let sol = Solution()
+sol.singleNumber([1,2,1,3,2,5]) // [3,5]
