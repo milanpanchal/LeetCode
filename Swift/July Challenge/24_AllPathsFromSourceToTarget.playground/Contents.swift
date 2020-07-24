4404d27b41a77ce8bf2b53ca62b27215708d553f
Milan Panchal <sam07it22@gmail.com>
2020-07-24 23:08:25 +0530

    feat: Problem Statement for July Challenge: Day 24 - All Paths From Source to Target

---
+++ Swift/July Challenge/24_AllPathsFromSourceToTarget.playground/Contents.swift
@@ -0,0 +1,44 @@
+/*:
+#   All Paths From Source to Target
+  https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3400/
+
+ ---
+
+### Problem Statement:
+ Given a directed, acyclic graph of `N` nodes.  Find all possible paths from node `0` to node `N-1`, and return them in any order.
+
+ The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+
+
+### Example:
+
+ ```
+ Input: [[1,2], [3], [3], []]
+ Output: [[0,1,3],[0,2,3]]
+ Explanation: The graph looks like this:
+ 
+ 0--->1
+ |    |
+ v    v
+ 2--->3
+ 
+ There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+
+ ```
+ 
+ ### Notes:
+ + The number of nodes in the graph will be in the range `[2, 15]`.
+ + You can print different paths in any order, but you should keep the order of nodes inside one path.
+ 
+ */
+
+
+import UIKit
+
+class Solution {
+    func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
+        
+    }
+}
+
+let sol = Solution()
