60e0a2f27c773f02ad5115d558defb719058c098
Milan Panchal <sam07it22@gmail.com>
2020-07-24 23:50:51 +0530

    feat: Solution for July Challenge: Day 24 - All Paths From Source to Target

--- Swift/July Challenge/24_AllPathsFromSourceToTarget.playground/Contents.swift
+++ Swift/July Challenge/24_AllPathsFromSourceToTarget.playground/Contents.swift
@@ -35,10 +35,69 @@
 
 import UIKit
 
+// 26 / 26 test cases passed.
+// Status: Accepted
+// Runtime: 152 ms
+// Memory Usage: 21.7 MB
+
 class Solution {
     func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
         
+        var result = [[Int]]()
+        var path = [0] // path always starts from node 0
+        dfs(graph, 0, &path, &result)
+        return result
+
+    }
+    
+    private func dfs(_ graph: [[Int]], _ node: Int, _ path: inout [Int], _ result: inout [[Int]]) {
+        if node == graph.count - 1 { // path ends at node N - 1
+            result.append(path)
+        }
+
+        for n in graph[node] {
+            path.append(n)
+            dfs(graph, n, &path, &result)
+            path.removeLast() // backtracking
+        }
+    }
+
+}
+
+class Solution1 {
+    
+    var seenPaths = [Int: [[Int]]]()
+
+    func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
+        
+        guard graph.count > 0 else {
+            return [[]]
+        }
+        
+        return allPaths(from: 0, to: graph.count - 1, graph: graph)
+    }
+        
+    func allPaths(from: Int, to: Int, graph: [[Int]]) -> [[Int]] {
+       
+        if from == to {
+            return [[to]]
+        }
+        
+        if let paths = seenPaths[from] {
+            return paths
+        }
+        
+        var paths = [[Int]]()
+        for next in graph[from] {
+            let nextPaths = allPaths(from: next, to: to, graph:graph)
+            paths += nextPaths
+        }
+        
+        let fromPaths = paths.map { [from] + $0 }
+        seenPaths[from] = fromPaths
+        return fromPaths
     }
 }
 
 let sol = Solution()
+sol.allPathsSourceTarget([[1,2],[3],[3],[]])
