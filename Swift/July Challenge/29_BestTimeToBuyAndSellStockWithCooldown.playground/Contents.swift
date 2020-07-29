619be00aac6baf12300d549228584b0458c621b2
Milan Panchal <sam07it22@gmail.com>
2020-07-29 22:15:20 +0530

    feat: Problem Statement for July Challenge: Day 29 - Best Time to Buy and Sell Stock with Cooldown

---
+++ Swift/July Challenge/29_BestTimeToBuyAndSellStockWithCooldown.playground/Contents.swift
@@ -0,0 +1,37 @@
+/*:
+# Best Time to Buy and Sell Stock with Cooldown
+  https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/548/week-5-july-29th-july-31st/3405/
+
+ ---
+
+### Problem Statement:
+
+ Say you have an array for which the i`th` element is the price of a given stock on day i.
+
+ Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+ + You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ + After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+
+### Example:
+
+ ```
+ Input: [1,2,3,0,2]
+ Output: 3
+ Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+ ```
+
+ */
+
+
+import UIKit
+
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        
+    }
+}
+
+let sol = Solution()
+sol.maxProfit([1,2,3,0,2]) // 3
