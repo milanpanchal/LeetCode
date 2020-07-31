58a3b9cba990ba39559702ee069057051feeb286
Milan Panchal <sam07it22@gmail.com>
2020-07-31 23:20:50 +0530

    feat: Solution for July Challenge: Day 31 - Climbing Stairs

--- Swift/July Challenge/31_ClimbingStairs.playground/Contents.swift
+++ Swift/July Challenge/31_ClimbingStairs.playground/Contents.swift
@@ -47,12 +47,27 @@
 import UIKit
 
 class Solution {
+    
+//    45 / 45 test cases passed.
+//    Status: Accepted
+//    Runtime: 4 ms
+//    Memory Usage: 20.7 MB
+
     func climbStairs(_ n: Int) -> Int {
+        var n = n
+        var a  = 1, b = 1
+        while n > 0 {
+            (a, b) = (b, a + b)
+            n -= 1
+        }
         
+        return a
     }
 }
 
 let sol = Solution()
 sol.climbStairs(2) // 2
 sol.climbStairs(3) // 3
-
+sol.climbStairs(4) // 5
+sol.climbStairs(5) // 8
+sol.climbStairs(6) // 13
