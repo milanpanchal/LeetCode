0df11d170c222fbdd52cd7eb029542894d898157
Milan Panchal <sam07it22@gmail.com>
2020-08-03 16:31:14 +0530

    fix: Solution for August Challenge: Day 3 - Valid Palindrome

--- Swift/August Challenge/03_ValidPalindrome.playground/Contents.swift
+++ Swift/August Challenge/03_ValidPalindrome.playground/Contents.swift
@@ -29,11 +29,72 @@
 
 import UIKit
 
+// 481 / 481 test cases passed.
+// Status: Accepted
+// Runtime: 64 ms
+// Memory Usage: 26.3 MB
+
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
+
+        if( s.isEmpty || s.count == 1 ) {
+            return true
+        }
         
+        let result = s.lowercased().replacingOccurrences( of:"[^0-9a-z]", with: "", options: .regularExpression)
+        
+
+        let sArray = Array(result)
+        var left = 0, right = sArray.count - 1
+        
+        
+        while left < right {
+            if sArray[left] != sArray[right] {
+                return false
+            }
+            left += 1
+            right -= 1
+        }
+        
+        return true
     }
+    
+    func isPalindrome3(_ s: String) -> Bool {
+        
+        if( s.isEmpty || s.count == 1 ) {
+            return true
+        }
+                
+        let s = Array(s)
+        var left = 0
+        var right = s.count - 1
+        
+        while left < right {
+            
+            while !(s[left].isNumber || s[left].isLetter), left < right {
+                left += 1
+            }
+
+            while !(s[right].isNumber || s[right].isLetter), left < right {
+                right -= 1
+            }
+            
+            if s[left].lowercased() != s[right].lowercased() {
+                return false
+            }
+            
+            left += 1
+            right -= 1
+        }
+        
+        return true
+    }
+
 }
 
 let sol = Solution()
-sol.isPalindrome("")
+sol.isPalindrome("") // true
+sol.isPalindrome("A man, a plan, a canal: Panama") // true
+sol.isPalindrome("race a car") // false
+sol.isPalindrome("0P") // false
+sol.isPalindrome("a") // true
