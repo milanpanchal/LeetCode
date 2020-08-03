11fa3b9e9f14398788d67836f1a63e245f0ede98
Milan Panchal <sam07it22@gmail.com>
2020-08-03 16:00:46 +0530

    feat: Problem Statement for August 03 - Valid Palindrome

---
+++ Swift/August Challenge/03_ValidPalindrome.playground/Contents.swift
@@ -0,0 +1,39 @@
+/*:
+# Valid Palindrome
+  https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3411/
+
+ ---
+
+### Problem Statement:
+ Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+### Example:
+
+ ```
+ Input: "A man, a plan, a canal: Panama"
+ Output: true
+
+ Input: "race a car"
+ Output: false
+
+ ```
+ 
+ ### Constraints:
+ + s consists only of printable ASCII characters.
+
+ ### Notes:
+ + For the purpose of this problem, we define empty string as valid palindrome.
+ 
+ */
+
+
+import UIKit
+
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        
+    }
+}
+
+let sol = Solution()
+sol.isPalindrome("")
