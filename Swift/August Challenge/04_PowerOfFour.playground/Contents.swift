2c4ad60b61640748f30484f1ebe38070e55ba6dc
Milan Panchal <sam07it22@gmail.com>
2020-08-04 19:06:33 +0530

    feat: Solution for August Challenge: Day 4 - Power of Four

---
+++ Swift/August Challenge/04_PowerOfFour.playground/Contents.swift
@@ -0,0 +1,41 @@
+/*:
+# Power of Four
+  https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3412/
+
+ ---
+
+### Problem Statement:
+
+ Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+
+### Example:
+
+ ```
+ Input: 16
+ Output: true
+
+ Input: 5
+ Output: false
+
+ ```
+ 
+ ### Follow up:
+ + Could you solve it without loops/recursion?
+ 
+ */
+
+
+import UIKit
+
+class Solution {
+    func isPowerOfFour(_ num: Int) -> Bool {
+        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0
+    }
+}
+
+let sol = Solution()
+sol.isPowerOfFour(16)
+sol.isPowerOfFour(5)
+sol.isPowerOfFour(8)
+
