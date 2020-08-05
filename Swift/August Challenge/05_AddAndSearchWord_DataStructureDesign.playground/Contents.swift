510544f5cb9a2fa8c8c472d69c8776cd2c4f251b
Milan Panchal <sam07it22@gmail.com>
2020-08-05 18:52:28 +0530

    feat: Problem Statement for August 05 - Add and Search Word - Data structure design

---
+++ Swift/August Challenge/05_AddAndSearchWord_DataStructureDesign.playground/Contents.swift
@@ -0,0 +1,68 @@
+/*:
+# Add and Search Word - Data structure design
+  https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3413/
+
+ ---
+
+### Problem Statement:
+
+ Design a data structure that supports the following two operations:
+
+ + `void addWord(word)`
+ + `bool search(word)`
+
+ search(word) can search a literal word or a regular expression string containing only letters `a-z` or `.`. A `.` means it can represent any one letter.
+
+
+
+
+### Example:
+
+ ```
+ addWord("bad")
+ addWord("dad")
+ addWord("mad")
+ search("pad") -> false
+ search("bad") -> true
+ search(".ad") -> true
+ search("b..") -> true
+
+ ```
+
+ ### Notes:
+ + You may assume that all words are consist of lowercase letters `a-z`.
+ 
+ ### Hint:
+ + You should be familiar with how a Trie works. If not, please work on this problem: [Implement Trie (Prefix Tree)](https://leetcode.com/problems/implement-trie-prefix-tree/) first.
+
+ */
+
+
+import UIKit
+
+
+class WordDictionary {
+
+    /** Initialize your data structure here. */
+    init() {
+        
+    }
+    
+    /** Adds a word into the data structure. */
+    func addWord(_ word: String) {
+        
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    func search(_ word: String) -> Bool {
+        
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * let obj = WordDictionary()
+ * obj.addWord(word)
+ * let ret_2: Bool = obj.search(word)
+ */
+
