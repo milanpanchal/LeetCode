848883a86e00aeb9f34286a0a682cce9f29a7b26
Milan Panchal <sam07it22@gmail.com>
2020-08-05 19:11:19 +0530

    feat: Solution for August 05 - Add and Search Word - Data structure design

--- Swift/August Challenge/05_AddAndSearchWord_DataStructureDesign.playground/Contents.swift
+++ Swift/August Challenge/05_AddAndSearchWord_DataStructureDesign.playground/Contents.swift
@@ -40,9 +40,71 @@
 
 import UIKit
 
+class TrieNode {
+    var children = [Character: TrieNode]()
+    var word: String? = nil
+}
+
+// 13 / 13 test cases passed.
+// Status: Accepted
+// Runtime: 624 ms
+// Memory Usage: 27.4 MB
 
 class WordDictionary {
 
+    let root = TrieNode()
+    var set = Set<String>()
+    
+    func addTrie(_ word: String) {
+         var p = root
+        
+         for c in word {
+             if p.children[c] == nil {
+                 p.children[c] = TrieNode()
+             }
+             p = p.children[c]!
+         }
+        
+        p.word = word
+     }
+    
+    func find(_ arr: [Character], _ str: String, _ index: Int, _ node: TrieNode) -> Bool {
+        
+        if index == arr.count { return false }
+        
+        let char = arr[index]
+        
+        if char != "." {
+            guard let child = node.children[char] else {
+                return false
+            }
+            
+            if let word = child.word, word.count == arr.count, index == arr.count - 1 {
+                return true
+            }
+            
+            if find(arr, str, index+1, child) {
+                return true
+            }
+            
+        } else {
+            for item in node.children {
+                let child = item.value
+                
+                if let word = child.word, word.count == arr.count, index == arr.count - 1 {
+                    return true
+                }
+
+                if find(arr, str, index+1, child) {
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
+
+    
     /** Initialize your data structure here. */
     init() {
         
@@ -50,19 +112,68 @@ class WordDictionary {
     
     /** Adds a word into the data structure. */
     func addWord(_ word: String) {
-        
+        addTrie(word)
     }
     
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     func search(_ word: String) -> Bool {
-        
+        if set.contains(word) { return true }
+        let arr = Array(word)
+        let res = find(arr, word, 0, root)
+        if res == true {
+            set.insert(word)
+        }
+        return res
     }
 }
 
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * let obj = WordDictionary()
- * obj.addWord(word)
- * let ret_2: Bool = obj.search(word)
- */
+
+class WordDictionary2 {
+    var wordSet: Set<String>
+    var charCountMap: [Int: [[Character]]]
+    
+    /** Initialize your data structure here. */
+    init() {
+        wordSet = []
+        charCountMap = [:]
+    }
+    
+    /** Adds a word into the data structure. */
+    func addWord(_ word: String) {
+        wordSet.insert(word)
+        charCountMap[word.count, default: []].append(Array(word))
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    func search(_ word: String) -> Bool {
+        if wordSet.contains(word) {
+            return true
+        }
+        let word = Array(word)
+        if let options = charCountMap[word.count] {
+            optionLoop: for option in options {
+                for i in 0..<word.count {
+                    if option[i] != word[i] && word[i] != Character(".") {
+                      continue optionLoop
+                    }
+                }
+                return true
+            }
+        }
+        return false
+    }
+}
+
+
+// Your WordDictionary object will be instantiated and called as such:
+
+let obj = WordDictionary()
+obj.addWord("bad")
+obj.addWord("dad")
+obj.addWord("mad")
+obj.search("pad") // false
+obj.search("bad") // true
+obj.search(".ad") // true
+obj.search("b..") // true
+
 
