35359ff6f5d1eadb956b5d63a324e7e9d7711b1a
Milan Panchal <sam07it22@gmail.com>
2020-08-11 23:54:25 +0530

    feat: Solution for August Challeng 11 & Problem Number 274 = H-Index

--- Swift/Problems/274_H-Index.playground/Contents.swift
+++ Swift/Problems/274_H-Index.playground/Contents.swift
@@ -1,6 +1,6 @@
 /*:
-# H-Index
-  https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3420/
+# 274. H-Index [Medium]
+  https://leetcode.com/problems/h-index/
 
  ---
 
