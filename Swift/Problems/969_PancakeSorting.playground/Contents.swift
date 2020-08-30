52ed9ed33732440e1fa42f663cd4712d4251d82a
Milan Panchal <sam07it22@gmail.com>
2020-08-30 23:30:26 +0530

    feat: Solution for Problem #969 - Pancake Sorting

--- Swift/Problems/969_PancakeSorting.playground/Contents.swift
+++ Swift/Problems/969_PancakeSorting.playground/Contents.swift
@@ -1,3 +1,113 @@
+/*:
+# 969. Pancake Sorting [Medium]
+  https://leetcode.com/problems/pancake-sorting/
+
+ ---
+
+### Problem Statement:
+
+ Given an array of integers `A`, We need to sort the array performing a series of pancake flips.
+
+ In one pancake flip we do the following steps:
+
+ + Choose an integer `k` where `0 <= k < A.length`.
+ + Reverse the sub-array `A[0...k]`.
+ 
+ For example, if `A = [3,2,1,4]` and we performed a pancake flip choosing `k = 2`, we reverse the sub-array `[3,2,1]`, so `A = [1,2,3,4]` after the pancake flip at `k = 2`.
+
+ Return an array of the k-values of the pancake flips that should be performed in order to sort` A`. Any valid answer that sorts the array within `10 * A.length` flips will be judged as correct.
+
+
+### Example 1:
+
+ ```
+ Input: A = [3,2,4,1]
+ Output: [4,2,4,3]
+ Explanation:
+ We perform 4 pancake flips, with k values 4, 2, 4, and 3.
+ Starting state: A = [3, 2, 4, 1]
+ After 1st flip (k = 4): A = [1, 4, 2, 3]
+ After 2nd flip (k = 2): A = [4, 1, 2, 3]
+ After 3rd flip (k = 4): A = [3, 2, 1, 4]
+ After 4th flip (k = 3): A = [1, 2, 3, 4], which is sorted.
+ Notice that we return an array of the chosen k values of the pancake flips.
+
+ ```
+ 
+ ### Example 2:
+
+ ```
+ Input: A = [1,2,3]
+ Output: []
+ Explanation: The input is already sorted, so there is no need to flip anything.
+ Note that other answers, such as [3, 3], would also be accepted.
+
+ ```
+ 
+ ### Constraints:
+ + 1 <= A.length <= 100
+ + 1 <= A[i] <= A.length
+ + All integers in `A` are unique (i.e. `A` is a permutation of the integers from `1` to `A.length`).
+ 
+ */
+
+
 import UIKit
 
-var str = "Hello, playground"
+class Solution {
+    func pancakeSort(_ A: [Int]) -> [Int] {
+        var a:[Int] = A
+        var result: [Int] = []
+        
+        var isArraySorted = isSorted(a)
+        var j: Int = a.count-1
+        
+        while !isArraySorted {
+            let currMaxIndex = getCurrMaxIndex(a, j)
+            flip(&a, currMaxIndex)
+            result.append(currMaxIndex+1)
+            flip(&a, j)
+            result.append(j+1)
+            j -= 1
+            isArraySorted = isSorted(a)
+        }
+        
+        return result
+    }
+    
+    private func getCurrMaxIndex(_ a: [Int], _ len: Int) -> Int {
+        var index = 0
+        for i in 0..<len {
+            if a[index] < a[i+1] {
+                index = i+1
+            }
+        }
+        return index
+    }
+    
+    private func flip(_ a: inout [Int], _ i: Int) {
+        var start: Int = 0
+        var end: Int = i
+        
+        while start < end {
+            let temp = a[start]
+            a[start] = a[end]
+            a[end] = temp
+            start += 1
+            end -= 1
+        }
+    }
+    
+    private func isSorted(_ a: [Int]) -> Bool {
+        for i in 0..<a.count-1 where a[i] > a[i+1] {
+            return false
+        }
+        return true
+    }
+}
+
+
+let sol = Solution()
+sol.pancakeSort([3,2,4,1])
+sol.pancakeSort([1,2,3])
+
